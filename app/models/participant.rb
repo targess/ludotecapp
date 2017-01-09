1c0ad8e50b094ffb6a90f8a98053c6a18dabb340
Jose Toscano <josetoscanogil@gmail.com>
2017-01-09 12:33:45 +0100

    model - participant refactor

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,15 +3,15 @@ class Participant < ApplicationRecord
   belongs_to :tournament
 
   validates_presence_of :player, :tournament
-  validates_uniqueness_of :player, :scope => :tournament
-  validate :max_participants_rearched, :must_have_at_least_tournament_minage, on: :create
+  validates_uniqueness_of :player, scope: :tournament
+  validate :tournament_must_have_free_participants_slots, :must_have_at_least_tournament_minage, on: :create
   validate :tournament_must_have_not_deleted_boardgame
 
   before_create :supplent_when_competitors_reached
   after_destroy :supplent_to_competitor_whe_competitor_destroyed
 
   def toggle_confirmed
-    self.confirmed ? (self.confirmed = false) : (self.confirmed = true)
+    confirmed ? (self.confirmed = false) : (self.confirmed = true)
   end
 
   def at_future_tournament
@@ -20,36 +20,29 @@ class Participant < ApplicationRecord
 
   private
 
-  def max_participants_rearched
-    return nil unless tournament.present?
-    if tournament.participants.count >= tournament.max_participants
-      errors.add(:tournament, "max participants are rearched")
-    end
+  def tournament_must_have_free_participants_slots
+    return false unless tournament.present?
+    errors.add(:tournament, "max participants are rearched") if tournament.max_participants_rearched
   end
 
   def must_have_at_least_tournament_minage
-    return nil unless tournament.present? & player.present?
+    return false unless tournament.present? & player.present?
     errors.add(:tournament, "player under minimal age") if player.age < tournament.minage
   end
 
   def supplent_when_competitors_reached
-    return nil unless tournament.present?
-     if (tournament.get_competitors.count) >= tournament.max_competitors
-      self.substitute = true
-     end
+    return false unless tournament.present?
+    self.substitute = true if tournament.get_competitors.count >= tournament.max_competitors
   end
 
   def supplent_to_competitor_whe_competitor_destroyed
-    unless substitute
-      first_substitute = tournament.get_substitutes.first
-      if first_substitute.present?
-        first_substitute.update(substitute: false)
-      end
-    end
+    return false unless substitute
+    first_substitute = tournament.get_substitutes.first
+    first_substitute.update(substitute: false) if first_substitute.present?
   end
 
   def tournament_must_have_not_deleted_boardgame
-    return nil unless tournament.present?
+    return false unless tournament.present?
     boardgame = tournament.boardgame
     errors.add(:tournament, "tournament with invalid boardgame") unless boardgame.deleted_at.nil?
   end
