79dfe3eea7f94b31e8b8ff4cd4f4596c976729a2
Jose Toscano <josetoscanogil@gmail.com>
2017-04-03 20:34:46 +0200

    add user migration belongs to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210093837) do
+ActiveRecord::Schema.define(version: 20170403183131) do
 
   create_table "boardgames", force: :cascade do |t|
     t.string   "name"
@@ -135,7 +135,9 @@ ActiveRecord::Schema.define(version: 20170210093837) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
