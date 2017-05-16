3580e29627a778d4c8cfd2babee5532cca0af2eb
Jose Toscano <josetoscanogil@gmail.com>
2017-05-16 20:38:09 +0200

    scoped admin players to current_organization

--- app/controllers/admin/boardgames_controller.rb
+++ app/controllers/admin/boardgames_controller.rb
@@ -1,10 +1,10 @@
 class Admin::BoardgamesController < ApplicationController
   def index
-    @boardgames = Boardgame.order(name: :asc).page(params[:page])
+    @boardgames = current_boardgames.order(name: :asc).page(params[:page])
   end
 
   def show
-    @boardgame = Boardgame.find_by(id: params[:id])
+    @boardgame = current_boardgames.find_by(id: params[:id])
   end
 
   def new
@@ -12,38 +12,36 @@ class Admin::BoardgamesController < ApplicationController
   end
 
   def edit
-    @boardgame = Boardgame.find_by(id: params[:id])
+    @boardgame = current_boardgames.find_by(id: params[:id])
   end
 
   def create
     @boardgame = Boardgame.new(boardgame_params)
+    @boardgame.organization = current_organization
     if @boardgame.save
-      redirect_to admin_boardgame_url(@boardgame), notice: 'Boardgame was successfully created.'
+      redirect_to admin_boardgame_url(@boardgame), notice: "Boardgame was successfully created."
     else
       render :new
     end
   end
 
   def import_from_bgg
-    @boardgame = Boardgame.new
-    if params[:search].present?
-      @boardgames = Boardgame.bgg_search_by_name(params[:search][:keywords])
-    else
-      @boardgames = []
-    end
+    @boardgame  = Boardgame.new
+    @boardgames = params[:search].present? ? Boardgame.bgg_search_by_name(params[:search][:keywords]) : []
   end
 
   def create_from_bgg
     boardgame = Boardgame.new_from_bgg_id(params[:id], params[:name])
+    boardgame.organization = current_organization
     if boardgame.save
-      redirect_to admin_boardgame_url(boardgame), notice: 'Boardgame was successfully created.'
+      redirect_to admin_boardgame_url(boardgame), notice: "Boardgame was successfully created."
     else
-      redirect_to :import_from_bgg, alert: 'Boardgame fails to import form BGG'
+      redirect_to :import_from_bgg, alert: "Boardgame fails to import form BGG"
     end
   end
 
   def update
-    @boardgame = Boardgame.find_by(id: params[:id])
+    @boardgame = current_boardgames.find_by(id: params[:id])
     if @boardgame.update(boardgame_params)
       redirect_to admin_boardgame_url(@boardgame)
     else
@@ -52,14 +50,22 @@ class Admin::BoardgamesController < ApplicationController
   end
 
   def destroy
-    @boardgame = Boardgame.find_by(id: params[:id])
+    @boardgame = current_boardgames.find_by(id: params[:id])
     @boardgame.destroy
     redirect_to admin_boardgames_path
   end
 
   private
 
-    def boardgame_params
-      params.require(:boardgame).permit(:name, :thumbnail, :image, :description, :minplayers, :maxplayers, :playingtime, :minage, :bgg_id, :barcode, :internalcode, :organization_id)
-    end
+  def boardgame_params
+    params.require(:boardgame).permit(:name, :thumbnail, :image, :description, :minplayers, :maxplayers, :playingtime, :minage, :bgg_id, :barcode, :internalcode, :organization_id)
+  end
+
+  def current_organization
+    current_user.organization
+  end
+
+  def current_boardgames
+    current_organization.boardgames
+  end
 end
