d68ed16a9d7084cc63401d08af3690d03a4fe33a
Jose Toscano <josetoscanogil@gmail.com>
2017-05-16 20:38:03 +0200

    scoped admin players to current_organization

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -1,10 +1,10 @@
 class Admin::PlayersController < ApplicationController
   def index
-    @players = Player.page(params[:page])
+    @players = current_players.page(params[:page])
   end
 
   def show
-    @player = Player.find_by(id: params[:id])
+    @player = current_players.find_by(id: params[:id])
   end
 
   def new
@@ -12,20 +12,21 @@ class Admin::PlayersController < ApplicationController
   end
 
   def edit
-    @player = Player.find_by(id: params[:id])
+    @player = current_players.find_by(id: params[:id])
   end
 
   def create
     @player = Player.new(player_params)
+    @player.organization = current_organization
     if @player.save
-      redirect_to admin_player_url(@player), notice: 'User was successfully created.'
+      redirect_to admin_player_url(@player), notice: "User was successfully created."
     else
       render :new
     end
   end
 
   def update
-    @player = Player.find_by(id: params[:id])
+    @player = current_players.find_by(id: params[:id])
     if @player.update(player_params)
       redirect_to admin_player_url(@player)
     else
@@ -34,15 +35,22 @@ class Admin::PlayersController < ApplicationController
   end
 
   def destroy
-    @player = Player.find_by(id: params[:id])
+    @player = current_players.find_by(id: params[:id])
     @player.destroy
     redirect_to admin_players_path
   end
 
-
   private
 
-    def player_params
-      params.require(:player).permit(:dni, :firstname, :lastname, :city, :province, :birthday, :email, :phone)
-    end
+  def player_params
+    params.require(:player).permit(:dni, :firstname, :lastname, :city, :province, :birthday, :email, :phone)
+  end
+
+  def current_organization
+    current_user.organization
+  end
+
+  def current_players
+    current_organization.players
+  end
 end
