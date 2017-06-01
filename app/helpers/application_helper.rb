1e3d4025ef87ad3a51fc35f164383ba655f21c52
Jose Toscano <josetoscanogil@gmail.com>
2017-06-01 20:53:06 +0200

    fix helpers return with empty vars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,12 @@ module ApplicationHelper
   end
 
   def players_number(min, max)
-    (min == max) ? min : "#{min} - #{max}"
+    return max unless min.present?
+    min == max ? max : "#{min} - #{max}"
   end
 
   def min_age(age)
-    age.to_s+"+"
+    age.to_s + "+" unless age.blank?
   end
 
   def beauty_date(date)
@@ -23,10 +24,10 @@ module ApplicationHelper
     "#{city} (#{province})" if city && province
   end
 
-  def img_boardgame(boardgame, size = "thumbnail" )
-    if size == "thumbnail"
+  def img_boardgame(boardgame, size = "thumbnail")
+    if size == "thumbnail" && boardgame[:thumbnail].present?
       image_tag(boardgame[:thumbnail], class: "img-responsive thumb64", alt: boardgame[:name])
-    elsif size == "image"
+    elsif size == "image" && boardgame[:image].present?
       image_tag(boardgame[:image], class: "img-responsive", alt: boardgame[:name])
     end
   end
