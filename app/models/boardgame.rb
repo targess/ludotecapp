972f4759138c8c235a7dbe1dc0388fe3a45680dc
Jose Toscano <josetoscanogil@gmail.com>
2017-06-08 21:31:41 +0200

    remove bgg parser from boardgames model

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,5 +1,4 @@
 class Boardgame < ApplicationRecord
-  require 'bgg'
 
   has_and_belongs_to_many :events
   has_many :loans
@@ -68,53 +67,4 @@ class Boardgame < ApplicationRecord
     errors.add(:destroy, "Cannot delete boardgames with pending loans") unless free_to_loan?
     throw(:abort) unless free_to_loan?
   end
-
-    def self.bgg_search_by_name(name = "los colonos de catan")
-      return [] if name.match(/^\s*$/) || name.length <= 3
-      searched_boardgames = BggApi::search("query=#{name}")
-      return [] unless searched_boardgames['total'].to_i > 0
-
-      searched_boardgames['item'].map do |boardgame|
-        {id: boardgame["id"], name: boardgame["name"].first["value"]}
-      end
-    end
-
-    def self.bgg_get_by_id(id)
-      boardgame = BggApi::thing("id=#{id}")['item']
-      boardgame.first if boardgame
-    end
-
-    def self.bgg_get_collection(username)
-      boardgames = BggApi::collection("username=#{username}&own=1")
-
-      return [] unless boardgames && boardgames['totalitems'].to_i.positive?
-
-      boardgames['item'].map do |boardgame|
-        {id: boardgame["objectid"], name: boardgame["name"].first["content"]}
-      end
-    end
-
-    def self.new_from_bgg_id(id, name=nil)
-      boardgame = self.bgg_get_by_id(id)
-      Boardgame.new(
-        name:        name || boardgame['name'].first['value'],
-        image:       boardgame['image'].first,
-        thumbnail:   boardgame['thumbnail'].first,
-        description: boardgame['description'].first,
-        minplayers:  boardgame['minplayers'].first['value'],
-        maxplayers:  boardgame['maxplayers'].first['value'],
-        playingtime: boardgame['playingtime'].first['value'],
-        minage:      boardgame['minage'].first['value'],
-        bgg_id:      boardgame['id'])
-    end
-
-    def self.import_from_bgg_collection(username, organization)
-      collection = bgg_get_collection(username)
-      collection.each do |boardgame|
-        boardgame = new_from_bgg_id(boardgame[:id], boardgame[:name])
-        boardgame.organization = organization
-        boardgame.save
-        sleep(2)
-      end
-    end
 end
