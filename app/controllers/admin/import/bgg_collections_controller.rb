495e298526c563b7ba64c17e6717ddff63e35672
Jose Toscano <josetoscanogil@gmail.com>
2017-06-12 19:37:29 +0200

    add import collection action

--- app/controllers/admin/import/bgg_collections_controller.rb
+++ app/controllers/admin/import/bgg_collections_controller.rb
@@ -6,5 +6,17 @@ class Admin::Import::BggCollectionsController < ApplicationController
   end
 
   def create
+    @boardgames = BggParser::ImportCollectionService.perform(params[:bgg_user], current_organization)
+    if @boardgames
+      redirect_to admin_boardgames_path, notice: "Collection was successfully imported."
+    else
+      redirect_to :index, alert: "Collection fails to import from BGG"
+    end
+  end
+
+  private
+
+  def current_organization
+    current_user.organization
   end
 end
