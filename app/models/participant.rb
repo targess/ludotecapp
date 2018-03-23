bd777c0116fd7f4bfc34c728e4e0b5ab6279a663
Jose Toscano <josetoscanogil@gmail.com>
2018-03-23 20:00:28 +0100

    added participants validations

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,7 +4,9 @@ class Participant < ApplicationRecord
 
   validates_presence_of :player, :tournament
   validates_uniqueness_of :player, scope: :tournament
-  validate :tournament_must_have_free_participants_slots, :must_have_at_least_tournament_minage, on: :create
+  validate :tournament_must_have_free_participants_slots,
+           :must_have_at_least_tournament_minage,
+           :tournament_must_be_active, on: :create
   validate :tournament_must_have_not_deleted_boardgame
 
   before_create :supplent_when_competitors_reached
@@ -26,39 +28,44 @@ class Participant < ApplicationRecord
   private
 
   def tournament_must_have_free_participants_slots
-    return false unless tournament.present?
+    return unless tournament.present?
     errors.add(:tournament, "max participants are rearched") if tournament.max_participants_rearched
   end
 
   def must_have_at_least_tournament_minage
-    return false unless tournament.present? & player.present?
+    return unless tournament.present? & player.present?
     errors.add(:tournament, "player under minimal age") if player.age.blank? || player.age < tournament.minage
   end
 
-  def supplent_when_competitors_reached
-    return false unless tournament.present?
-    self.substitute = true if tournament.competitors.count >= tournament.max_competitors
-  end
-
-  def supplent_to_competitor_when_competitor_destroyed
-    return false if substitute
-    first_substitute = tournament.substitutes.first
-    first_substitute.update(substitute: false) if first_substitute.present?
+  def tournament_must_be_active
+    return unless tournament.present?
+    errors.add(:tournament, "Cannot add participant to finished tournament") unless at_future_tournament?
   end
 
   def tournament_must_have_not_deleted_boardgame
-    return false unless tournament.present?
+    return unless tournament.present?
     boardgame = tournament.boardgame
     errors.add(:tournament, "tournament with invalid boardgame") unless boardgame.deleted_at.nil?
   end
 
+  def supplent_when_competitors_reached
+    return unless tournament.present?
+    self.substitute = true if tournament.competitors.count >= tournament.max_competitors
+  end
+
+  def supplent_to_competitor_when_competitor_destroyed
+    return if substitute
+    first_substitute = tournament.substitutes.first
+    first_substitute.update(substitute: false) if first_substitute.present?
+  end
+
   def not_removed_from_past_tournament
-    errors.add(:destroy, "Cannot delete participant from past tournament") unless at_future_tournament?
+    errors.add(:participant, "Cannot delete participant from past tournament") unless at_future_tournament?
     throw(:abort) unless at_future_tournament?
   end
 
   def player_and_tournament_must_be_at_same_event
-    return false unless tournament.present? & player.present?
+    return unless tournament.present? & player.present?
     errors.add(:tournament, "belongs to another event") unless player.events.include?(tournament.event)
   end
 end
